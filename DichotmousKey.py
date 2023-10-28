a2a831000e7694763c6eb14b66b6991185039388
Henry "TJ" Chen <127168121+LatinScribe@users.noreply.github.com>
2023-10-27 23:40:37 -0400

    Update DichotmousKey.py

--- DichotmousKey.py
+++ DichotmousKey.py
@@ -13,112 +13,114 @@ EXIT = "5"
 
 # first option
 def seeds():
-    # clear the screen
-    os.system('cls')
-    print("Fruits and Vegetables Dichotomous Key")
-    seeds = input("Does it have seeds?\n1. yes 2. no: ")
-    if seeds == YES:
-        red()
-    elif seeds == NO:
-        print("It is a vegetable.")
-        # You're not supposed to let the program end, so ask the user to select an option instead of return
-        return
-    elif seeds == EXIT:
-        sys.exit()
+# clear the screen
+os.system('cls')
+print("Fruits and Vegetables Dichotomous Key")
+seeds = input("Does it have seeds?\n1. yes 2. no: ")
+if seeds == YES:
+    red()
+elif seeds == NO:
+    print("It is a vegetable.")
+    # You're not supposed to let the program end, so ask the user to select an option instead of return
+    return
+elif seeds == EXIT:
+    sys.exit()
 
-    else:
-        # Maybe ask the user to retry, it's up to you
-        print("Unexeptected input")
+else:
+    # Maybe ask the user to retry, it's up to you
+    print("Unexeptected input")
 
 
 # second option
 def red():
-    # clear the screen
-    os.system('cls')
-    red = input("Is it red?\n1. yes 2. no: ")
-    # old code
-    #   if red == option1:
-    #     return round()
-    #   if red == option2:
-    #     orange()
-    #   return red
-    # red(option1, option2)
+# clear the screen
+os.system('cls')
+red = input("Is it red?\n1. yes 2. no: ")
+# old code
+#   if red == option1:
+#     return round()
+#   if red == option2:
+#     orange()
+#   return red
+# red(option1, option2)
 
-    if red == YES:
-        myround()
-    elif red == NO:
-        orange()
+if red == YES:
+    myround()
+elif red == NO:
+    orange()
 
-    elif red == PREV:
-        seeds()
-    elif red == START_OVER:
-        seeds()
-    elif red == EXIT:
-        sys.exit()
+elif red == PREV:
+    seeds()
+elif red == START_OVER:
+    seeds()
+elif red == EXIT:
+    sys.exit()
 
-    else:
-        print("Unexeptected input")
+else:
+    print("Unexeptected input")
 
 
 def orange():
-    # clear the screen
-    os.system('cls')
-    orange = input("Is it orange?\n1. yes 2. no")
-    # if orange == "1":
-    #   print("It is an orange.")
-    #   sys.exit()
-    # if orange == "2":
-    #   return yellow()
+# clear the screen
+os.system('cls')
+orange = input("Is it orange?\n1. yes 2. no")
+# if orange == "1":
+#   print("It is an orange.")
+#   sys.exit()
+# if orange == "2":
+#   return yellow()
 
-    if orange == YES:
-        print("It is an orange.")
-        # You're not supposed to let the program end, so ask the user to select an option instead of return
-        return
-    elif orange == NO:
-        # call yellow here - implement yellow yourself
-        # yellow()
-        pass
+if orange == YES:
+    print("It is an orange.")
+    # You're not supposed to let the program end, so ask the user to select an option instead of return
+    return
+elif orange == NO:
+    # call yellow here - implement yellow yourself
+    # yellow()
+    pass
 
-    elif orange == PREV:
-        red()
-    elif orange == START_OVER:
-        seeds()
-    elif orange == EXIT:
-        sys.exit()
+elif orange == PREV:
+    red()
+elif orange == START_OVER:
+    seeds()
+elif orange == EXIT:
+    sys.exit()
 
-    else:
-        print("Unexeptected input")
+else:
+    print("Unexeptected input")
 
 
 def myround():
-    # clear the screen
-    os.system('cls')
-    round = input("Is it round?\n1. yes 2. no")
-    # if round == option1:
-    #   print("It is an apple.")
-    #   sys.exit()
-    # if round == option2:
-    #   print("It is a strawberry.")
-    #   sys.exit()
+# clear the screen
+os.system('cls')
+round = input("Is it round?\n1. yes 2. no")
+# if round == option1:
+#   print("It is an apple.")
+#   sys.exit()
+# if round == option2:
+#   print("It is a strawberry.")
+#   sys.exit()
 
-    if round == YES:
-        print("It is an apple.")
-        # You're not supposed to let the program end, so ask the user to select an option instead of return
-        return
-    elif round == NO:
-        print("It is a strawberry.")
-        # You're not supposed to let the program end, so ask the user to select an option instead of return
-        return
+if round == YES:
+    print("It is an apple.")
+    # You're not supposed to let the program end, so ask the user to select an option instead of return
+    return
+elif round == NO:
+    print("It is a strawberry.")
+    # You're not supposed to let the program end, so ask the user to select an option instead of return
+    return
 
-    elif round == PREV:
-        red()
-    elif round == START_OVER:
-        seeds()
-    elif round == EXIT:
-        sys.exit()
+elif round == PREV:
+    red()
+elif round == START_OVER:
+    seeds()
+elif round == EXIT:
+    sys.exit()
 
-    else:
-        print("Unexeptected input")
+else:
+    print("Unexeptected input")
+
+
 
 # Other stuff, implement it yourself following the abouve
 # def yellow():
